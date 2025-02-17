ce86cf69d9058b96eced13e47e32bdeafb89f7fa
gilbruno <gilles_bruno@gmx.fr>
2025-02-17 16:16:23 +0100

    modify testimonials

--- src/components/Testimonials/Testimonials.tsx
+++ src/components/Testimonials/Testimonials.tsx
@@ -21,22 +21,22 @@ const Testimonials = () => {
     </div>
     <div className={styles.containerTestimonials}>
       <div className={styles.containerTestimonial1}>
-        <Image src="/img/testimonials/nadine_leprince.jpg" 
-          alt="Nadine Le Prince - Artiste contemporain TOP 3 I-CAC - Témoignage sur InRealArt" 
+        <Image src="/img/testimonials/marc_pfelzer.jpg" 
+          alt="Marc Pfelzer - Artiste contemporain TOP 3 I-CAC - Témoignage sur InRealArt" 
           width={150} height={150} className={styles.testimonialImage} />
         <div className={styles.wrapperText2}>
           <div className={styles.title2}>
-            <div className={styles.artist1}>Nadine Le Prince</div>
+            <div className={styles.artist1}>Marc Pfelzer</div>
           </div>
           <div className={styles.artistText1}>
               <span className={styles.artistText11}>
                 {parse(i18n[language].testimonials.artist1)}
                 <br />
               </span>
-              <span className={styles.artistText13}>{parse(i18n[language].testimonials.ranking)}</span>
-              <span
-                className={styles.artistText14}>
-                 TOP 3 I-CAC
+              <span className={styles.artistText13}>{parse(i18n[language].testimonials.styleArtist1)}</span>
+              &nbsp;
+              <span className={styles.artistText14}>
+                {parse(i18n[language].testimonials.rankingArtist1)}
                 <br />
               </span>
           </div>
@@ -58,9 +58,10 @@ const Testimonials = () => {
                 <br />
                 <br />
               </span>
-              <span className={styles.artistText22}> {parse(i18n[language].testimonials.ranking)}</span>
+              <span className={styles.artistText22}> {parse(i18n[language].testimonials.styleArtist2)}</span>
+              &nbsp;
               <span className={styles.artistText23}>
-                 TOP 7 I-CAC
+              {parse(i18n[language].testimonials.rankingArtist2)}
               </span>
             </span>
           </div>
